6a25015bb345616ee73fb5fc00d1360057970b6a
Panagiotis Koutsourakis <kutsurak@ekt.gr>
2015-03-07 01:16:35 +0200

    Implemented find_components function
    Turned the logic to a function that returns a list of components for
    further processing

--- labels.py
+++ labels.py
@@ -7,22 +7,25 @@ def show_image(img):
     cv2.imshow('result', img), cv2.waitKey(0)
 
 
-if __name__ == '__main__':
-    image = cv2.imread('./data/FBs.tif')
+def find_components(image):
+    """
+    Given a binary image this function returns a list of images containing the connected components
+    """
+
     img = cv2.split(image)[0]
-    show_image(img)
 
     labels = measure.label(img, background=0)
-    print("Number of labels = ", np.max(labels))
+    # print("Number of labels = ", np.max(labels))
     label_number = 0
+    components = []
     while True:
-        temp = np.uint8(labels==label_number) * 255
+        temp = np.uint8(labels == label_number) * 255
         if not cv2.countNonZero(temp):
             break
+
         t = cv2.findNonZero(temp).T
         (min_i, max_i, min_j, max_j) = (np.min(t[0]), np.max(t[0]), np.min(t[1]), np.max(t[1]))
-
-        show_image(temp[min_j:max_j, min_i:max_i])
+        components.append(temp[min_j:max_j, min_i:max_i])
         label_number += 1
 
-    cv2.destroyAllWindows()
+    return components
