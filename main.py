d89f8b550e4944b1c0701dc84e28f9c826764c0c
Panagiotis Koutsourakis <kutsurak@ekt.gr>
2015-03-09 23:02:24 +0200

    Read 32bit tif image

--- main.py
+++ main.py
@@ -2,42 +2,25 @@ import cv2
 from components import find_components
 from matplotlib import pyplot
 import numpy as np
+from PIL import Image
 
 
-def write_output_images(cmps):
-    image_name = './data/FBs.tif'
-    original_image_name = './data/Im_crop.tif'
-
-    image = cv2.imread(image_name)
-    original_image = cv2.imread(original_image_name)
+def write_bounding_box_images(cmps, image, original_image):
+    # image_name = './data/FBs.tif'
+    # original_image_name = './data/Im_crop.tif'
+    #
+    # image = cv2.imread(image_name)
+    # original_image = cv2.imread(original_image_name)
 
     for cmp in cmps:
         cv2.rectangle(original_image, (cmp[1][0], cmp[1][1]), (cmp[1][2], cmp[1][3]), (255, 0, 0), 4)
         cv2.rectangle(image, (cmp[1][0], cmp[1][1]), (cmp[1][2], cmp[1][3]), (255, 0, 0), 4)
 
-
-    # pyplot.imshow(original_image)
-    # pyplot.xticks([]), pyplot.yticks([])
-    # pyplot.show()
-
-    pyplot.imshow(image)
-    pyplot.show()
-
     cv2.imwrite('./output/bounding_box.png', original_image)
     cv2.imwrite('./output/bounding_box_blob.png', image)
 
 
-def main():
-    image_name = './data/FBs.tif'
-    original_image_name = './data/Im_crop.tif'
-
-
-    image = cv2.imread(image_name)
-    original_image = cv2.imread(original_image_name)
-
-
-    cmps = find_components(image)
-
+def write_min_area_rect_images(cmps, image, original_image):
     idx = 0
     for cmp in cmps:
         non_zero = cv2.findNonZero(cmp[0])
@@ -47,20 +30,30 @@ def main():
         box = np.int0(box)
         cv2.drawContours(image, [box], idx, (255, 0, 0), 3)
         cv2.drawContours(original_image, [box], idx, (255, 0, 0), 3)
-        #print(box)
-        #cv2.rectangle(image, )
-
-    # pyplot.imshow(image)
-    # pyplot.xticks([]), pyplot.yticks([])
-    # pyplot.show()
-    #
-    # pyplot.imshow(original_image)
-    # pyplot.show()
 
     cv2.imwrite('./output/min_area_box.png', original_image)
     cv2.imwrite('./output/min_area_box_blob.png', image)
 
-    write_output_images(cmps)
+
+def main():
+    image_name = './data/FBs.tif'
+    original_image_name = './data/Im_crop.tif'
+    dsm_image_name = './data/DSM_crop.tif'
+
+    print("Reading images")
+    image = cv2.imread(image_name)
+    original_image = cv2.imread(original_image_name)
+    dsm_image = np.asarray(Image.open(dsm_image_name)) # Use PILLOW to read 32bit tif
+
+
+    print("Computing components")
+    cmps = find_components(image)
+
+    print("Writing bounding boxes")
+    write_bounding_box_images(cmps, image.copy(), original_image.copy())
+
+    print("Writing minimum area rectangles")
+    write_min_area_rect_images(cmps, image.copy(), original_image.copy())
 
 
 if __name__ == '__main__':
