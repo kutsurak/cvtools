d43254e85d8662a89ce9dc55ffb3f12a532e116d
Panagiotis Koutsourakis <kutsurak@ekt.gr>
2015-03-10 16:26:16 +0200

    Added optimization factory

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import cv2
-from components import find_components
-from matplotlib import pyplot
 import numpy as np
 from PIL import Image
+from matplotlib import pyplot
+from scipy.optimize import minimize, anneal, basinhopping
+
+from components import find_components
+from optimization import make_cost_function
+
+
+def get_components(cmps, image):
+    image_comps = []
+    for cmp in cmps:
+        image_comps.append(image[cmp[1][1]:cmp[1][3], cmp[1][0]:cmp[1][2]])
+
+    return image_comps
 
 
 def write_bounding_box_images(cmps, image, original_image):
-    # image_name = './data/FBs.tif'
-    # original_image_name = './data/Im_crop.tif'
-    #
-    # image = cv2.imread(image_name)
-    # original_image = cv2.imread(original_image_name)
-
     for cmp in cmps:
         cv2.rectangle(original_image, (cmp[1][0], cmp[1][1]), (cmp[1][2], cmp[1][3]), (255, 0, 0), 4)
-        cv2.rectangle(image, (cmp[1][0], cmp[1][1]), (cmp[1][2], cmp[1][3]), (255, 0, 0), 4)
+        ret = cv2.rectangle(image, (cmp[1][0], cmp[1][1]), (cmp[1][2], cmp[1][3]), (255, 0, 0), 4)
 
     cv2.imwrite('./output/bounding_box.png', original_image)
     cv2.imwrite('./output/bounding_box_blob.png', image)
@@ -45,7 +50,6 @@ def main():
     original_image = cv2.imread(original_image_name)
     dsm_image = np.asarray(Image.open(dsm_image_name)) # Use PILLOW to read 32bit tif
 
-
     print("Computing components")
     cmps = find_components(image)
 
@@ -54,6 +58,26 @@ def main():
 
     print("Writing minimum area rectangles")
     write_min_area_rect_images(cmps, image.copy(), original_image.copy())
+
+    image_comps = get_components(cmps, image.copy())
+    print('Total components', len(image_comps))
+
+    i = 0
+    results = []
+    #for img in image_comps:
+    img = image_comps[7]
+    print("minimizing affine for component", i)
+    cost_function = make_cost_function(img)
+    aff0 = np.array([1, 0, 0, 1, 0, 0])
+    # res = anneal(cost_function, aff0, schedule='fast')
+    res = minimize(cost_function, aff0, method='anneal', options={'disp': True, 'ftol': 1e-4})
+    #res = basinhopping(cost_function, aff0)
+    print(res.x)
+    i += 1
+    results.append(res)
+
+
+
 
 
 if __name__ == '__main__':
---
+++ optimization.py
@@ -0,0 +1,36 @@
+from math import floor
+import numpy as np
+import cv2
+import scipy as sp
+
+
+def make_cost_function(image):
+    r = 0.6
+    width = image.shape[1]
+    height = image.shape[0]
+    x0, x1 = 0, width - 1
+    y0, y1 = int(floor((height - r*height)/2.0)), int(floor((height - r*height)/2.0 + r*height) - 1)
+    canonical_image = np.zeros(image.shape, np.uint8)
+    cv2.rectangle(canonical_image, (x0, y0), (x1, y1), (255, 255, 255), -1)
+
+    def make_affine_matrix(transformation):
+        M = np.asarray([[transformation[0], transformation[1], transformation[2]],
+                        [transformation[3], transformation[4], transformation[5]]], np.float32)
+
+        return M
+
+    def cost_function(transformation):
+        error_sum = 0
+        m = make_affine_matrix(transformation)
+
+        for index, v in np.ndenumerate(image):
+            affine_canonical = cv2.warpAffine(canonical_image, m, (canonical_image.shape[0], canonical_image.shape[1]))
+            if 0 <= index[0] < affine_canonical.shape[0] and 0 <= index[1] < affine_canonical.shape[1]:
+                if affine_canonical[index[0], index[1], 0] != v:
+                    error_sum += 1
+            else:
+                error_sum += 1
+
+        return error_sum
+
+    return cost_function
